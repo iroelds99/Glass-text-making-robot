06dd1c556f30fe0585cdf6ceee4a0802e5f59582
ImSaleza <125234718+iroelds99@users.noreply.github.com>
2023-11-20 01:51:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,6 @@ $dbname = "mydatabase";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
